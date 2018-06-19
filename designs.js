d766b946470a54d823092da72b2a42fea93ac715
Elena Kovrigina <elena.kovrigina@me.com>
2018-06-19 13:06:16 -0400

    Change input type on "submit" and add event.preventDefault(); in js

--- designs.js
+++ designs.js
@@ -3,36 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-	let row = parseInt(document.getElementById("inputHeight").value);
-	let column = parseInt(document.getElementById("inputWidth").value);
-	let table = document.getElementById("pixelCanvas");
 
-	while(table.hasChildNodes()) {
-        table.removeChild(table.firstChild);
-    }
+function makeGrid(e) {
+    event.preventDefault();
+    const width = document.getElementById('inputWidth').value;
+    const height = document.getElementById('inputHeight').value; 
+    const canvas = document.getElementById('pixelCanvas');
+    canvas.innerHTML = '';
 
-	for (let i=0; i<row; ++i) {
-		let tr = table.appendChild(document.createElement('tr'));
-		for (let j=0; j<column; ++j) {
-			let td = tr.appendChild(document.createElement('td'));
-            //td.id = "cell"+ i + j;
-            td.addEventListener('click', changeColor);
-
-          /*  function changeColor() {
-                let color = document.getElementById("colorPicker").value;
-                this.style.backgroundColor = color;
-                return false;
-            }*/
-
-            
+for(var i = 0; i<height; i++) {
+    const row = canvas.appendChild(document.createElement('tr'));
+        for (var j=0; j<width; j++) {
+            const cell = row.appendChild(document.createElement('td'));
+            cell.addEventListener('click', changeColor);
             function changeColor(){
                 let color = document.getElementById("colorPicker").value;
-                $("tr").eq(i).find("td").eq(j).css("background-color", color);
-                //td.style.backgroundColor = color;
+               // $("tr").eq(i).find("td").eq(j).css("background-color", color);
+                cell.style.backgroundColor = color;
             };
-        }
-    
-    }
+            }
+        
 }
+} 
+
 
